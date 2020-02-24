0463df0a7d1c326d1ed86c7a99bad9590e646c33
lll <m18858403302@163.com>
2020-02-24 20:02:28 +0800

    20.00

--- src/main/java/live/community/community/controller/IndexController.java
+++ src/main/java/live/community/community/controller/IndexController.java
@@ -4,15 +4,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class HelloController {
+public class IndexController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name ="name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @GetMapping("/")
+    public String index(){
+
+        return "index";
 
     }
 
